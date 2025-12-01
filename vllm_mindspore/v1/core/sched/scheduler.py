b5f4b9d93584e51ddf95dfcbdaeb3930da342c8d
r1chardf1d0 <xiaruijie@huawei.com>
2025-12-01 17:47:02 +0800

    fix update_from_output

--- vllm_mindspore/v1/core/sched/scheduler.py
+++ vllm_mindspore/v1/core/sched/scheduler.py
@@ -22,34 +22,32 @@
 # noqa: G004
 
 from collections import defaultdict
-from typing import Optional
 
 from vllm.logger import init_logger
-from vllm.v1.core.sched.output import SchedulerOutput
-from vllm.v1.core.sched.utils import check_stop
+from vllm.v1.core.sched.utils import check_stop, remove_all
 from vllm.v1.engine import EngineCoreOutput, EngineCoreOutputs, FinishReason
-from vllm.v1.outputs import ModelRunnerOutput
 from vllm.v1.request import Request, RequestStatus
-from vllm.v1.spec_decode.metrics import SpecDecodingStats
 
 logger = init_logger(__name__)
 
 
-# TODO: check if 0.11.0 need
 def update_from_output(
     self,
-    scheduler_output: SchedulerOutput,
-    model_runner_output: ModelRunnerOutput,
+    scheduler_output,
+    model_runner_output,
 ) -> dict[int, EngineCoreOutputs]:
     sampled_token_ids = model_runner_output.sampled_token_ids
-    spec_token_ids = model_runner_output.spec_token_ids
     logprobs = model_runner_output.logprobs
     prompt_logprobs_dict = model_runner_output.prompt_logprobs_dict
     num_scheduled_tokens = scheduler_output.num_scheduled_tokens
+    pooler_outputs = model_runner_output.pooler_output
+    num_nans_in_logits = model_runner_output.num_nans_in_logits
+    kv_connector_output = model_runner_output.kv_connector_output
 
-    new_running: list[Request] = []
     outputs: dict[int, list[EngineCoreOutput]] = defaultdict(list)
-    spec_decoding_stats: Optional[SpecDecodingStats] = None
+    spec_decoding_stats = None
+    kv_connector_stats = (kv_connector_output.kv_connector_stats
+                          if kv_connector_output else None)
 
     # Add by vllm-mindspore begin:
     running_req_ids = [req.request_id for req in self.running]
@@ -58,11 +56,20 @@ def update_from_output(
     abort_req_ids: list[str] = []
     # Add by vllm-mindspore end.
 
-    # NOTE(woosuk): As len(self.running) can be up to 1K or more, the below
-    # loop can be a performance bottleneck. We should do our best to avoid
-    # expensive operations inside the loop.
-    for request in self.running:
-        req_id = request.request_id
+    # NOTE(woosuk): As len(num_scheduled_tokens) can be up to 1K or more,
+    # the below loop can be a performance bottleneck. We should do our best
+    # to avoid expensive operations inside the loop.
+    stopped_running_reqs: set[Request] = set()
+    stopped_preempted_reqs: set[Request] = set()
+    for req_id, num_tokens_scheduled in num_scheduled_tokens.items():
+        assert num_tokens_scheduled > 0
+        request = self.requests.get(req_id)
+        if request is None:
+            # The request is already finished. This can happen if the
+            # request is aborted while the model is executing it (e.g.,
+            # in pipeline parallelism).
+            continue
+
         # Add by vllm-mindspore begin:
         # None sampled_token_ids comes from exception model execution,
         # set them to abort list
@@ -75,102 +82,78 @@ def update_from_output(
                 EngineCoreOutput(request_id=req_id,
                                  new_token_ids=[],
                                  finish_reason=FinishReason.ABORT,
-                                 new_logprobs=None,
-                                 new_prompt_logprobs_tensors=None,
                                  stop_reason=request.stop_reason,
                                  events=request.take_events()))
             abort_req_ids.append(req_id)
             continue
         # Add by vllm-mindspore end.
 
-        num_tokens_scheduled = num_scheduled_tokens.get(req_id, 0)
-        if num_tokens_scheduled == 0:
-            # The request was not scheduled in this step.
-            new_running.append(request)
-            continue
-
         req_index = model_runner_output.req_id_to_index[req_id]
-        generated_token_ids = sampled_token_ids[req_index]
+        generated_token_ids = sampled_token_ids[
+            req_index] if sampled_token_ids else []
 
         scheduled_spec_token_ids = (
             scheduler_output.scheduled_spec_decode_tokens.get(req_id))
         if scheduled_spec_token_ids:
+            num_draft_tokens = len(scheduled_spec_token_ids)
+            num_accepted = len(generated_token_ids) - 1
+            num_rejected = num_draft_tokens - num_accepted
             # num_computed_tokens represents the number of tokens
             # processed in the current step, considering scheduled
             # tokens and rejections. If some tokens are rejected,
             # num_computed_tokens is decreased by the number of rejected
-            # tokens, where is given by:
-            # len(scheduled_spec_token_ids) + 1 - len(generated_token_ids).
-            num_tokens_rejected = (len(scheduled_spec_token_ids) + 1 -
-                                   len(generated_token_ids))
-            request.num_computed_tokens -= num_tokens_rejected
+            # tokens.
+            request.num_computed_tokens -= num_rejected
             spec_decoding_stats = self.make_spec_decoding_stats(
                 spec_decoding_stats,
-                num_draft_tokens=len(scheduled_spec_token_ids),
-                num_accepted_tokens=len(generated_token_ids) - 1)
-
-        cached_encoder_input_ids = (
-            self.encoder_cache_manager.get_cached_input_ids(request))
-        # OPTIMIZATION: Avoid list(set) if the set is empty.
-        if cached_encoder_input_ids:
-            for input_id in list(cached_encoder_input_ids):
-                mm_positions = request.mm_positions[input_id]
-                start_pos = mm_positions.offset
-                num_tokens = mm_positions.length
-                if start_pos + num_tokens <= request.num_computed_tokens:
-                    # The encoder output is already processed and stored
-                    # in the decoder's KV cache.
-                    self.encoder_cache_manager.free_encoder_input(
-                        request, input_id)
+                num_draft_tokens=num_draft_tokens,
+                num_accepted_tokens=num_accepted)
 
         stopped = False
         new_logprobs = None
         new_token_ids = generated_token_ids
         kv_transfer_params = None
+        status_before_stop = request.status
 
-        # Append generated tokens and check for stop. Note that if
-        # a request is still being prefilled, we expect the model runner
-        # to return empty token ids for the request.
-        for num_new, output_token_id in enumerate(new_token_ids, 1):
-            request.append_output_token_ids(output_token_id)
+        # Check for stop and update request status.
+        if new_token_ids:
+            new_token_ids, stopped = self._update_request_with_output(
+                request, new_token_ids)
 
-            # Check for stop and update request state.
-            # This must be called before we make the EngineCoreOutput.
-            stopped = check_stop(request, self.max_model_len)
-            if stopped:
-                kv_transfer_params = self._free_request(request)
-                del new_token_ids[num_new:]  # Trim new tokens if needed.
-                break
+        # Stop checking for pooler models.
+        pooler_output = None
+        if pooler_outputs:
+            pooler_output = pooler_outputs[req_index]
+            stopped = check_stop(request, self.max_model_len, pooler_output)
+
+        if stopped:
+            kv_transfer_params = self._free_request(request)
+            if status_before_stop == RequestStatus.RUNNING:
+                stopped_running_reqs.add(request)
+            else:
+                stopped_preempted_reqs.add(request)
 
         # Extract sample logprobs if needed.
-        if request.sampling_params.logprobs is not None and logprobs:
+        if request.sampling_params is not None \
+            and request.sampling_params.logprobs is not None and logprobs:
             # NOTE: once we support N tokens per step (spec decode),
             # the outer lists can be of length > 1.
             new_logprobs = logprobs.slice(req_index, req_index + 1)
 
         if new_token_ids and self.structured_output_manager.should_advance(
                 request):
-            """
-            NOTE: structured_output_request
-            should not be None if use_structured_output, we have
-            check above, so safe to ignore type warning
-            """
+            # should not be None if use_structured_output, we have
+            # checked above, so safe to ignore type warning
             request.structured_output_request.grammar.accept_tokens(  # type: ignore[union-attr]
                 req_id, new_token_ids)
 
-        # Add newly generated spec token ids to the request.
-        if spec_token_ids is not None:
-            if self.structured_output_manager.should_advance(request):
-                metadata = request.structured_output_request
-                # Needs to happen after new_token_ids are accepted.
-                request.spec_token_ids = metadata.grammar.validate_tokens(  # type: ignore[union-attr]
-                    spec_token_ids[req_index])
-            else:
-                request.spec_token_ids = spec_token_ids[req_index]
+        if num_nans_in_logits is not None and req_id in num_nans_in_logits:
+            request.num_nans_in_logits = num_nans_in_logits[req_id]
 
         # Get prompt logprobs for this request.
         prompt_logprobs_tensors = prompt_logprobs_dict.get(req_id)
-        if new_token_ids or kv_transfer_params:
+        if new_token_ids or pooler_output is not None \
+            or kv_transfer_params:
 
             # Add EngineCoreOutput for this Request.
             outputs[request.client_index].append(
@@ -180,18 +163,16 @@ def update_from_output(
                     finish_reason=request.get_finished_reason(),
                     new_logprobs=new_logprobs,
                     new_prompt_logprobs_tensors=prompt_logprobs_tensors,
+                    pooling_output=pooler_output,
                     stop_reason=request.stop_reason,
                     events=request.take_events(),
                     kv_transfer_params=kv_transfer_params,
+                    trace_headers=request.trace_headers,
                     num_cached_tokens=request.num_cached_tokens,
                 ))
-
         else:
             # Invariant: EngineCore returns no partial prefill outputs.
             assert not prompt_logprobs_tensors
-
-        if not stopped:
-            new_running.append(request)
 
     # Add by vllm-mindspore begin:
     # make failed requests finished to make the server
@@ -201,17 +182,17 @@ def update_from_output(
         self.finish_requests(abort_req_ids, RequestStatus.FINISHED_ABORTED)
     # Add by vllm-mindspore end.
 
+    # Remove the stopped requests from the running and waiting queues.
+    if stopped_running_reqs:
+        self.running = remove_all(self.running, stopped_running_reqs)
+    if stopped_preempted_reqs:
+        # This is a rare case and unlikely to impact performance.
+        self.waiting.remove_requests(stopped_preempted_reqs)
+
     # KV Connector: update state for finished KV Transfers.
-    self._update_from_kv_xfer_finished(model_runner_output)
-
-    # Return the cached request data to the queue so they can be reused.
-    for req_data in scheduler_output.scheduled_cached_reqs:
-        # NOTE(rob): since we free stopped reqs above, adding stopped reqs
-        # to _cached_reqs_data will cause a memory leak.
-        if req_data.req_id not in self.finished_req_ids:
-            self._cached_reqs_data[req_data.req_id].append(req_data)
-
-    self.running = new_running
+    if model_runner_output.kv_connector_output:
+        self._update_from_kv_xfer_finished(
+            model_runner_output.kv_connector_output)
 
     # Create EngineCoreOutputs for all clients that have requests with
     # outputs in this step.
@@ -233,9 +214,13 @@ def update_from_output(
                     finished_requests=finished_set)
         finished_req_ids.clear()
 
-    if engine_core_outputs:
+    if (stats := self.make_stats(spec_decoding_stats,
+                                 kv_connector_stats)) is not None:
         # Return stats to only one of the front-ends.
-        next(iter(engine_core_outputs.values())).scheduler_stats = (
-            self.make_stats(spec_decoding_stats))
+        if (eco := next(iter(engine_core_outputs.values()), None)) is None:
+            # We must return the stats even if there are no request
+            # outputs this step.
+            engine_core_outputs[0] = eco = EngineCoreOutputs()
+        eco.scheduler_stats = stats
 
     return engine_core_outputs
